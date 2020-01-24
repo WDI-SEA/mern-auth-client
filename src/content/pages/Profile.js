6791bbbf2563b5a142c79a3aef4e5b86f8a5e128
brandiw <brandimichelle.0911@gmail.com>
2020-01-24 14:32:37 -0800

    Adding code along up until afternoon break Friday

--- src/content/pages/Profile.js
+++ src/content/pages/Profile.js
@@ -1,6 +1,12 @@
 import React from 'react'
+import { Redirect } from 'react-router-dom'
 
 const Profile = props => {
+  // If there is not a user, send them away
+  if (!props.user) {
+    return <Redirect to="/" />
+  }
+  
   return (
     <div>
       <h2>Profile STUB</h2>
