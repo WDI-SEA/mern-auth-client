75f33215749d84e13fabc6b2a3b0b1add09bb9e2
brandiw <brandimichelle.0911@gmail.com>
2020-01-23 16:35:59 -0800

    Code along in progress

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // Import packages
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom'
 
 // Resources and custom components
@@ -13,13 +13,41 @@ const App = props => {
   // Declare state variables
   let [user, setUser] = useState(null)
 
+  // Define any onload actions (e.g., to look for the token)
+  useEffect(() => {
+    console.log('check for token!')
+    decodeToken()
+  }, [])
+
+  // Helper function to update the user (login, signup, and logout use this)
+  const updateUser = newToken => {
+    if (newToken) {
+      // Save the token 
+      localStorage.setItem('userToken', newToken)
+
+      // Update the user with the token's info
+      decodeToken(newToken)
+    }
+    else {
+      setUser(null)
+    }
+  }
+
+  // Helper function to decode existing tokens
+  const decodeToken = existingToken => {
+    let token = existingToken || localStorage.getItem('userToken')
+
+    console.log('The token is:', token)
+    // TODO: Decode token
+  }
+
   return (
     <Router>
       <div className="App">
-        <Nav />
+        <Nav user={user} updateUser={updateUser} />
         <Header />
         <main>
-          <Content />
+          <Content user={user} updateUser={updateUser} />
         </main>
         <Footer />
       </div>
